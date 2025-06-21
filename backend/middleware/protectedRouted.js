e384295207cd62ac1d6cc773920bcf4c3af890d2
Nihal Panday <nihalpanday2020@gmail.com>
2025-06-21 17:30:23 +0530

    middleware added

---
+++ backend/middleware/protectedRouted.js
@@ -0,0 +1,30 @@
+import jwt from "jsonwebtoken"
+import dotenv from "dotenv"
+import User from "../models/user.model"
+import { where } from "sequelize"
+
+dotenv.config()
+
+
+export const protectedRouted = async (req, res, next) => {
+    try {
+        const accessToken = req.cookies.accessToken
+
+        if (!accessToken) {
+            return res.status(401).json({ message: "Access token is required" })
+        }
+
+        const decoded = jwt.verify(accessToken, process.env.JWT_SECRET)
+        const user = await User.findOne({ where: { id: decoded.userId } })
+        if (!user) {
+            return res.status(400).json({ message: "Access token is required" })
+        }
+
+        req.user = user
+
+        next()
+    } catch (error) {
+        console.log("JWT middleware error:", error.message)
+        return res.status(401).json({ message: "Invalid or expired token", error: error.message })
+    }
+}
