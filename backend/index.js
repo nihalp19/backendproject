80796c56db9a81bae79737f13bc313b5b955fb77
Nihal Panday <nihalpanday2020@gmail.com>
2025-06-21 17:31:34 +0530

    index,auth routes and user model updated

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,7 @@
 import express from "express"
 import dotenv from "dotenv"
 import { sequelize } from "./utils/db.js"
-
+import cookieParser from "cookie-parser"
 import authRoutes from "./routes/auth.routes.js"
 
 dotenv.config()
@@ -11,6 +11,7 @@ const app = express()
 
 
 app.use(express.json())
+app.use(cookieParser())
 
 app.use("/api/auth",authRoutes)
 
--- backend/models/user.model.js
+++ backend/models/user.model.js
@@ -1,8 +1,13 @@
 import { DataTypes } from "sequelize";
-import { sequelize } from "../utils/db";
+import { sequelize } from "../utils/db.js";
 
 
 const User = sequelize.define('User', {
+    id: {
+        type: DataTypes.UUID,
+        defaultValue: DataTypes.UUIDV4,
+        primaryKey: true,
+    },
     name: {
         type: DataTypes.STRING,
         allowNull: false
