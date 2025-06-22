b20b5e9f7727dfbe7a6aa4e5a7a8e42717a98a78
Nihal Panday <nihalpanday2020@gmail.com>
2025-06-22 10:44:50 +0530

    generateToken fn  updated

--- backend/utils/generateToken.js
+++ backend/utils/generateToken.js
@@ -3,7 +3,7 @@ import dotenv from "dotenv"
 
 dotenv.config()
 
-export const generatToken = (userId) => {
+export const generateToken = (userId) => {
     const token = jwt.sign({ userId: userId }, process.env.JWT_SECRET, { expiresIn: "7d" })
     return token
 }
