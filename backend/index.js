f818837628f92a43594330c99a87846b139ce0b3
Nihal Panday <nihalpanday2020@gmail.com>
2025-06-22 10:44:15 +0530

    index updated

--- backend/index.js
+++ backend/index.js
@@ -3,6 +3,7 @@ import dotenv from "dotenv"
 import { sequelize } from "./utils/db.js"
 import cookieParser from "cookie-parser"
 import authRoutes from "./routes/auth.routes.js"
+import patientsRoutes from "./routes/patients.routes.js"
 
 dotenv.config()
 
@@ -14,10 +15,15 @@ app.use(express.json())
 app.use(cookieParser())
 
 app.use("/api/auth",authRoutes)
+app.use("/api/patients",patientsRoutes)
 
-sequelize.authenticate().then(() => {
-    console.log("DB CONNECTED")
-    app.listen(PORT, () => {
-        console.log(`SERVER STARTED AT PORT NO ${PORT}`)
-    })
-}).catch(err => console.error('Unable to connect to the database:', err))
+sequelize.sync({ alter: true })  // use { force: true } only for development to drop & recreate tables
+  .then(() => {
+    console.log('Database synced');
+    app.listen(8080, () => {
+      console.log('SERVER STARTED AT PORT NO 8080');
+    });
+  })
+  .catch(err => {
+    console.error('Failed to sync database:', err);
+  });
